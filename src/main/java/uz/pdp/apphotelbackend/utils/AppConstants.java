b67588b3b7290ee056b8df48b0f2dd50623955ba
Mehrojbek <mehrojbekdeveloper@gmail.com>
2025-01-27 21:40:21 +0500

    booking complete

--- src/main/java/uz/pdp/apphotelbackend/utils/AppConstants.java
+++ src/main/java/uz/pdp/apphotelbackend/utils/AppConstants.java
@@ -3,5 +3,8 @@ package uz.pdp.apphotelbackend.utils;
 public interface AppConstants {
 
     String BASE_PATH = "/api/v1";
+    String API_VERSION = "v1";
+    String API_VERSION2 = "v2";
+    String API_VERSION3 = "v3";
 
 }
